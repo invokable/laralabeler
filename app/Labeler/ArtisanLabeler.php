c40c246d3d1be0915080516cdd23922e0d21fff6
kawax <kawaxbiz@gmail.com>
2024-12-22 10:08:59 +0900

    Update composer.lock
    vercel
    
    wip
    
    Update README.md
    
    wip
    
    Update app.php
    
    Update app.php
    
    Update ArtisanLabeler.php

--- app/Labeler/ArtisanLabeler.php
+++ app/Labeler/ArtisanLabeler.php
@@ -4,13 +4,8 @@ declare(strict_types=1);
 
 namespace App\Labeler;
 
-use Revolution\AtProto\Lexicon\Attributes\Format;
-use Revolution\AtProto\Lexicon\Attributes\NSID;
-use Revolution\AtProto\Lexicon\Attributes\Output;
-use Revolution\AtProto\Lexicon\Attributes\Ref;
-use Revolution\AtProto\Lexicon\Attributes\Union;
-use Revolution\AtProto\Lexicon\Contracts\Com\Atproto\Label;
-use Revolution\AtProto\Lexicon\Contracts\Com\Atproto\Moderation;
+use Illuminate\Support\Facades\Config;
+use Revolution\Bluesky\Facades\Bluesky;
 use Revolution\Bluesky\Labeler\AbstractLabeler;
 use Revolution\Bluesky\Labeler\LabelDefinition;
 use Revolution\Bluesky\Labeler\LabelLocale;
@@ -19,21 +14,6 @@ class ArtisanLabeler extends AbstractLabeler
 {
     /**
      * Label definitions.
-     *
-     * ```
-     * use Revolution\Bluesky\Labeler\LabelDefinition;
-     *
-     * protected function labels() {
-     *     return [
-     *         new LabelDefinition(
-     *               identifier: 'artisan',
-     *               severity: 'inform',
-     *               blurs: 'none',
-     *               locales: [new LabelLocale(lang: 'en', name: 'artisan', description: 'Web artisan')],
-     *         ),
-     *     ];
-     * }
-     * ```
      *
      * @return array<LabelDefinition>
      */
@@ -52,22 +32,29 @@ class ArtisanLabeler extends AbstractLabeler
     }
 
     /**
+     * @return array{cursor: string, labels: array{ver?: int, src: string, uri: string, cid?: string, val: string, neg?: bool, cts: string, exp?: string, sig?: mixed}}
+     *
      * @link https://docs.bsky.app/docs/api/com-atproto-label-query-labels
      */
-    #[NSID(Label::queryLabels)]
-    #[Output(Label::queryLabelsResponse)]
-    public function queryLabels(array $uriPatterns, #[Format('did')] ?array $sources = null, ?int $limit = 50, ?string $cursor = null): array
+    public function queryLabels(array $uriPatterns, ?array $sources = null, ?int $limit = 50, ?string $cursor = null): array
     {
+        info('queryLabels', compact('uriPatterns', 'sources', 'limit', 'cursor'));
+
+        $res = Bluesky::getFollowers(Config::string('bluesky.labeler.identifier'));
+        info('getFollowers', $res->json());
+
         return [];
     }
 
     /**
+     * @return array{id: int, reasonType: string, reason: string, subject: array, reportedBy: string, createdAt: string}
+     *
      * @link https://docs.bsky.app/docs/api/com-atproto-moderation-create-report
      */
-    #[NSID(Moderation::createReport)]
-    #[Output(Moderation::createReportResponse)]
-    public function createReport(#[Ref('com.atproto.moderation.defs#reasonType')] string $reasonType, #[Union(['com.atproto.admin.defs#repoRef', 'com.atproto.repo.strongRef'])] array $subject, ?string $reason = null): array
+    public function createReport(string $reasonType, array $subject, ?string $reason = null): array
     {
+        info('createReport', compact('reasonType', 'subject', 'reason'));
+
         return [];
     }
 }
