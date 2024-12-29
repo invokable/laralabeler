b52428f7ea784eaf9d348e8c7a5cc465f17ae667
kawax <kawaxbiz@gmail.com>
2024-12-29 11:23:07 +0900

    Update FollowListener.php

--- app/Listeners/FollowListener.php
+++ app/Listeners/FollowListener.php
@@ -34,11 +34,13 @@ class FollowListener
             Labeler::log(self::class, $message);
 
             $labeler_session = cache('labeler_session');
-            Labeler::log('labeler_session', $labeler_session);
+            Labeler::log('labeler_session cache', $labeler_session);
 
             if (empty($labeler_session)) {
                 Bluesky::login(config('bluesky.labeler.identifier'), config('bluesky.labeler.password'));
                 $labeler_session = Bluesky::agent()->session()->toArray();
+                Labeler::log('labeler_session login', $labeler_session);
+
                 cache()->forever('labeler_session', $labeler_session);
             }
 
