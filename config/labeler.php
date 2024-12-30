38181b0106929e8c44c02c67905dec683d0cbc6b
kawax <kawaxbiz@gmail.com>
2024-12-30 10:02:14 +0900

    badge

--- config/labeler.php
+++ config/labeler.php
@@ -2,18 +2,21 @@
 
 return [
     'description' => '
+# laralabeler
+
 A sample of Bluesky **Labeler** made with Laravel.
 
 This Labeler just displays an "artisan" badge on your account (profile page & all posts).
+![badge](./artisan-badge.png)
 
 ## Add badge
-- **Subscribe** to this account as a Labeler.
+- **Subscribe** to this account ([@laralabeler.bsky.social](https://bsky.app/profile/laralabeler.bsky.social)) as a Labeler.
 - **Follow** this account.
 - The badge should appear immediately, if not wait about an hour.
 - Check that the badge has been displayed.
 
 ## Remove badge
-- **Unfollow** this account.
+- **Unfollow** this account ([@laralabeler.bsky.social](https://bsky.app/profile/laralabeler.bsky.social)).
 - Send an **"Appeal"** to the Labeler from your profile page.
 - Check that the badge has been removed.
 - If the badge has not been removed, wait a while and send "Appeal" again.
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
 <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
     <div
         class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
-        <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
+        <div class="relative w-full max-w-2xl px-6 lg:max-w-5xl">
             <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
                 <div class="flex lg:justify-center lg:col-start-2">
                     <svg class="h-12 w-auto lg:h-16 text-[#FF2D20]" viewBox="0 0 62 65" fill="none"
@@ -31,28 +31,29 @@
 
             <main class="mt-6">
                 <div class="mx-auto">
-                    <a
-                        href="https://bsky.app/profile/laralabeler.bsky.social"
+                    <div
                         class="flex flex-col items-center gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                     >
                         <div class="relative flex flex-col items-center gap-6">
                             <div class="flex items-center gap-6">
-                                <div class="prose pt-5">
-                                    <h2 class="text-xl font-semibold text-black dark:text-white">laralabeler</h2>
-
+                                <div class="prose prose-red pt-5">
                                     <p class="mt-4 text-sm/relaxed">
                                         {{ $markdown }}
                                     </p>
                                 </div>
                             </div>
 
-                            <svg class="size-6 shrink-0 stroke-[#FF2D20]" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                 viewBox="0 0 24 24" stroke-width="1.5">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"/>
-                            </svg>
+                            <a
+                                href="https://bsky.app/profile/laralabeler.bsky.social">
+                                <svg class="size-6 shrink-0 stroke-[#FF2D20]" xmlns="http://www.w3.org/2000/svg"
+                                     fill="none"
+                                     viewBox="0 0 24 24" stroke-width="1.5">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"/>
+                                </svg>
+                            </a>
                         </div>
-                    </a>
+                    </div>
                 </div>
             </main>
 
